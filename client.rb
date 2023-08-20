90093518c4f6482490a48a5a3c12099d3510d8cd
Rafael <rafaelnepomuceno99@gmail.com>
2023-08-20 16:16:02 -0300

    synchronization implemented

--- client.rb
+++ client.rb
@@ -1,1 +1,34 @@
-# to be done
+require 'socket'
+require 'time'
+
+HOST = '192.168.2.7'
+PORT = 1234
+
+client = TCPSocket.new(HOST, PORT)
+loop do
+  begin
+  #tempo do relogio do cliente
+  current_time = Time.now
+  #tempo da requisicao
+  t1 = Time.now
+  server_time = Time.parse(client.gets)
+  #tempo da resposta
+  t2 = Time.now
+  
+
+  round_trip_time = t2 - t1
+  adjusted_time = server_time + round_trip_time / 2
+
+  puts "-------------Sincronizacao-------------"
+  puts "Round trip time (RTT): #{round_trip_time} seconds"
+  puts "Tempo do servidor: #{server_time}"
+  puts "Tempo atual do lado do cliente: #{current_time}"
+  puts "Tempo ajustado: #{adjusted_time}"
+  puts "---------------------------------------"
+
+  sleep(5)
+  rescue Errno::EPIPE
+    puts "O servidor caiu"
+    break
+  end
+end
--- server.rb
+++ server.rb
@@ -1,1 +1,25 @@
-# to be done
+require 'socket'
+require 'time'
+
+HOST = '192.168.2.7'
+PORT = 1234
+
+server = TCPServer.new(HOST, PORT)
+puts "Server started on #{HOST}/#{PORT}"
+clients = []
+
+loop do
+  client = server.accept
+  clients << client
+  puts "Client connected"
+
+  if clients.size == 2
+    loop do
+      clients.each do |client|
+        client.puts(Time.now.iso8601(6))
+      end
+      puts Time.now
+      sleep(5)
+    end
+  end
+end
